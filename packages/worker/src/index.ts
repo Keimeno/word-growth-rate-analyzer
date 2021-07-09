7ff7ce476cde0c0fb43c4925f2d8d7c13c394558
Constantin Metz <constantin@metzworld.com>
2021-07-09 22:54:43 +0200

    removed worker package

--- packages/worker/src/index.ts
+++
@@ -1,6 +0,0 @@
-import {config} from 'dotenv';
-config({path: '../../.env'});
-
-import './lib';
-import './models';
-import './receiver';
--- packages/worker/src/lib/redis.ts
+++
@@ -1,7 +0,0 @@
-import IORedis from 'ioredis';
-
-const {REDIS_HOST, REDIS_PORT, REDIS_PASSWORD} = process.env;
-
-export const redis = new IORedis(+(REDIS_PORT + ''), REDIS_HOST, {
-  password: REDIS_PASSWORD,
-});
