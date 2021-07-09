c69543cd05cbd0d35a3806e0e87b0e54ace4d937
Constantin Metz <constantin@metzworld.com>
2021-07-09 23:20:49 +0200

    removed need for external worker service to upsert data

--- packages/scraper/src/publisher/data.ts
+++ packages/scraper/src/publisher/data.ts
@@ -1,13 +1,19 @@
-const scrapedWordsCount = [] as ScrapedWordCount[];
+import {
+  generateHourlyScrapedWordCountModel,
+  ScrapedWordCount,
+} from '@keimeno/wgra-common';
+import {connection} from '../infrastructure';
 
-export type ScrapedWordCount = {
-  readonly word: string;
-  count: number;
-};
+const HourlyScrapedWordCount = generateHourlyScrapedWordCountModel(connection);
+
+HourlyScrapedWordCount.init();
+
+const scrapedWordsCount: ScrapedWordCount[] = [];
 
 export const incrementScrapedWordCount = (word: string) => {
   // check if there's already a ScrapedWordCount object in the
   // scraped words count array
+  // ignore the createdAt date
   const index = scrapedWordsCount.findIndex(
     wordCount => wordCount.word === word
   );
@@ -19,23 +25,41 @@ export const incrementScrapedWordCount = (word: string) => {
   }
 
   // item not found, create new object with count 1
-  scrapedWordsCount.push({word, count: 1});
+  scrapedWordsCount.push({word, count: 1, createdAt: new Date()});
 };
 
-const publishScrapedWordsCount = async () => {
+const upsertScrapedWordsCount = async () => {
   try {
-    console.log(
-      `Successfully published the count of ${scrapedWordsCount.length} words`
+    // in order to remove inconsistencies between lesser occurring words,
+    // we must overwrite the createdAt date
+    const createdAt = new Date();
+    createdAt.setMinutes(0, 0, 0);
+
+    // create a deep clone of scrapedWordsCount
+    // a clone must be created and the scrapedWordsCount
+    // must be emptied later on, this has to be done since all upserts
+    // are running one by one. This would otherwise cause a concurrency issue
+    const wordsCount: ScrapedWordCount[] = JSON.parse(
+      JSON.stringify(scrapedWordsCount)
     );
 
     // empty scraped words count
     scrapedWordsCount.length = 0;
+
+    for (let i = 0; i < wordsCount.length; i++) {
+      const {word, count} = wordsCount[i];
+      await HourlyScrapedWordCount.updateOne(
+        {word, createdAt},
+        {$inc: {count}},
+        {upsert: true}
+      );
+    }
+
+    console.log(`Successfully upserted ${wordsCount.length} words`);
   } catch (e) {
-    console.log(e);
-    console.log('Failed to publish scrapedWordsCount, retrying...');
-    setTimeout(() => publishScrapedWordsCount(), 5000);
+    console.log('Failed to upsert scrapedWordsCount', e);
   }
 };
 
-// publishing every minute
-setInterval(() => publishScrapedWordsCount(), 1000 * 60);
+// upsert every minute
+setInterval(() => upsertScrapedWordsCount(), 1000 * 60);
